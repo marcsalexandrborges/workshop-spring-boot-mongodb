818a7ff31ca38b018189048f99e32ae7f0920187
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-03-16 14:41:33 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcsalexandrborges/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcsalexandrborges.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcsalexandrborges.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/marcsalexandrborges/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.marcsalexandrborges.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcsalexandrborges.workshopmongo.domain.User;
+import com.marcsalexandrborges.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll()	{
+		return repo.findAll();
+
+	}
+}
