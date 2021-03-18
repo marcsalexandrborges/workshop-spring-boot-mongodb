9e29a22aa86a900bf05fbaea19d0ef5e7dcbca0c
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-03-18 14:22:52 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/marcsalexandrborges/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.marcsalexandrborges.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.marcsalexandrborges.workshopmongo.domain.User;
+import com.marcsalexandrborges.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+    public void run(String...args0) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+}
