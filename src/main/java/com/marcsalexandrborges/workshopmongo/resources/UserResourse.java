77c4d1e54a62a316ec3dcd8c23d4839543a4add1
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-03-20 00:27:19 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/marcsalexandrborges/workshopmongo/resources/UserResourse.java
+++ src/main/java/com/marcsalexandrborges/workshopmongo/resources/UserResourse.java
@@ -45,5 +45,11 @@ public class UserResourse {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 		
 }
