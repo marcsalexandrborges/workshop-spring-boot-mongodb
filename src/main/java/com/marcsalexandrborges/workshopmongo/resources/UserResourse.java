ffc12de11ad9f630b85363fb2e30589cda738e2a
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-03-22 20:08:41 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/marcsalexandrborges/workshopmongo/resources/UserResourse.java
+++ src/main/java/com/marcsalexandrborges/workshopmongo/resources/UserResourse.java
@@ -51,5 +51,14 @@ public class UserResourse {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void>update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 		
 }
