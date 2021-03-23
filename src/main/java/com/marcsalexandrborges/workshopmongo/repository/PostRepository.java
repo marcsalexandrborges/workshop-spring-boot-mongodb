0adf51dcd63f0124429f6922e673e7bbca44b62b
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-03-23 15:14:29 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/marcsalexandrborges/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.marcsalexandrborges.workshopmongo.repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcsalexandrborges.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
