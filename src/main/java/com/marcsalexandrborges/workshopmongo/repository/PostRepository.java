8aace96bd1b89c0d1f9283e031bef25cba009643
Marcos Alexandre Boeges <marcsalexandr@gmail.com>
2021-04-20 19:14:21 -0300

    Consulta simples com @query

--- src/main/java/com/marcsalexandrborges/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcsalexandrborges/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.marcsalexandrborges.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcsalexandrborges.workshopmongo.domain.Post;
@@ -9,8 +10,8 @@ import com.marcsalexandrborges.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
-	List<Post> findByTitleContaining(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
